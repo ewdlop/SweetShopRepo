8348f1da1beb8084d2151008800cfc9d6761c4de
Raymond Lei <ray810815@gmail.com>
2019-05-13 21:20:38 -0400

    new subscription

---
+++ SweetShop/SweetShop/Migrations/20190514003723_newazure.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SweetShop.Migrations
+{
+    public partial class newazure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
