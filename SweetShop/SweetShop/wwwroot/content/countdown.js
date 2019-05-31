9a6ca04bf8e02905813aacc06aa2e3545eddf10f
Raymond Lei <ray810815@gmail.com>
2019-05-30 20:32:52 -0400

    countdown timer

---
+++ SweetShop/SweetShop/wwwroot/content/countdown.js
@@ -0,0 +1,24 @@
+﻿var countDownDate = new Date("Feb 23, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    var seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
