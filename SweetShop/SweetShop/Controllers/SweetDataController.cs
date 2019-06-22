ec995243e10a55afda3b7688141fa8a6a869f16a
Raymond Lei <ray810815@gmail.com>
2019-06-22 10:57:21 -0400

    trying to add json parsing sweet repository

---
+++ SweetShop/SweetShop/Controllers/SweetDataController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SweetShop.Models;
+using SweetShop.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SweetShop.Controllers
+{
+    [Route("api/[controller]")]
+    public class SweetDataController : Controller
+    {
+
+        private readonly ISweetRepository _sweetRepository;
+
+        public SweetDataController(ISweetRepository sweetRepository)
+        {
+            _sweetRepository = sweetRepository;
+        }
+        [HttpGet]
+        public IEnumerable<SweetViewModel> LoadMoreSweets()
+        {
+            IEnumerable<Sweet> dbSweets = null;
+            dbSweets = _sweetRepository.GetSweets().OrderBy(s => s.Id).Take(2);
+            List<SweetViewModel> sweets = new List<SweetViewModel>();
+            foreach(var dbSweet in dbSweets)
+            {
+                sweets.Add(MapDbSweetToSweetViewModel(dbSweet));
+            }
+            return sweets;
+        }
+        private SweetViewModel MapDbSweetToSweetViewModel(Sweet dbSweet)
+        {
+            return new SweetViewModel()
+            {
+                Id = dbSweet.Id,
+                Name = dbSweet.Name,
+                Price = dbSweet.Price,
+                ShortDescription = dbSweet.ShortDescription,
+                ImageThumbnailUrl = dbSweet.ImageThumbnailUrl,
+                CategoryId = dbSweet.CategoryId
+            };
+        }
+    }
+    
+
+
+}
---
+++ SweetShop/SweetShop/ViewModels/SweetViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SweetShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetShop.ViewModels
+{
+    public class SweetViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
